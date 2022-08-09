57b4dca2eb381d261bd4b550bfcaa1b478357192
Karl Wheezer D. God <110885234+InfernoKarl@users.noreply.github.com>
2022-08-08 23:17:56 -0700

    + Auto Chest Farm (working as of 8/8/2022)

---
+++ Scripts/chestfarm.lua
@@ -0,0 +1,15 @@
+getgenv().Chest = true
+while Chest do task.wait()
+
+for _,v in pairs(game:GetService("Workspace").Chests:GetDescendants()) do
+    if v.Name == "Gold Chest" and v.CanCollide == true then
+local Time = 1
+local tween =  game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(Time), {CFrame = v.CFrame})
+
+
+tween:Play()
+tween.Completed:Wait(E)
+fireclickdetector(v:FindFirstChild("Click"))
+end
+end
+end
