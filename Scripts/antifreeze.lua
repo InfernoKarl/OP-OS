aed23ceee1308493cffbdb0c512de89f2b27a5da
Karl Wheezer D. God <110885234+InfernoKarl@users.noreply.github.com>
2022-08-11 18:15:36 -0700

    + Anti Freeze for Tundra (working as of 11/8/2022)

--- Scripts/antifreeze.lua
+++ Scripts/antifreeze.lua
@@ -1,11 +1,11 @@
-getgenv().InfiniteStuff = true
+getgenv().AntiFreeze = true
 
 local Client = game:GetService("Players").LocalPlayer
 local PlayerValues = Client.PlayerGui
 local Heat = PlayerValues.Heat
 
 OldNewIndex = hookmetamethod(game, "__newindex", function(self, index, value)
-    if InfiniteStuff and checkcaller() == false then
+    if AntiFreeze and checkcaller() == false then
         if (self == Heat) and index == "Value" then
             value = 120
         end
