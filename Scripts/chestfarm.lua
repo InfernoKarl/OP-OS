2032c306a0652d2ab0e0146380f6e7c37cfe2464
Karl Wheezer D. God <110885234+InfernoKarl@users.noreply.github.com>
2022-08-28 19:12:47 -0700

    + Made Auto Chest Faster

--- Scripts/chestfarm.lua
+++ Scripts/chestfarm.lua
@@ -3,12 +3,12 @@ while Chest do task.wait()
 
 for _,v in pairs(game:GetService("Workspace").Chests:GetDescendants()) do
     if v.Name == "Gold Chest" and v.CanCollide == true then
-local Time = 1
+local Time = 0.5
 local tween =  game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(Time), {CFrame = v.CFrame})
 
 
 tween:Play()
-tween.Completed:Wait(E)
+tween.Completed:Wait(Time)
 fireclickdetector(v:FindFirstChild("Click"))
 end
 end
