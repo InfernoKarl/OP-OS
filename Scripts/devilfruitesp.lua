c7cd6c61d886160511368a74838115ba3d62b929
Karl Wheezer D. God <110885234+InfernoKarl@users.noreply.github.com>
2022-11-13 12:28:48 -0800

    Made devil fruit ESP refresh with new fruits
    doesn't have manually dropped fruits, will make that eventually

--- Scripts/devilfruitesp.lua
+++ Scripts/devilfruitesp.lua
@@ -1,8 +1,13 @@
+while wait(1) do
 for i,v in pairs(game.Workspace.DevilFruits:GetDescendants()) do
 if v.ClassName == "TouchTransmitter" then
 local BillboardGui = Instance.new("BillboardGui")
 local TextLabel = Instance.new("TextLabel")
 
+if v:FindFirstChild("BillboardGui") then
+    v.BillboardGui:Destroy()
+end
+      
 BillboardGui.Parent = v.Parent
 BillboardGui.AlwaysOnTop = true
 BillboardGui.LightInfluence = 1
@@ -16,5 +21,6 @@ TextLabel.Size = UDim2.new(1, 0, 1, 0)
 TextLabel.Text = v.Parent.Parent.Name
 TextLabel.TextColor3 = Color3.new(1, 0, 0)
 TextLabel.TextScaled = true
+end
 end
 end
